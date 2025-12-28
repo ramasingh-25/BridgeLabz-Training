f5781d94aef69ca779561987b947c9fef8b6c7ee
RamaSingh333 <swatisingh7763@gmail.com>
2025-12-28 23:22:30 +0530

    [Rama] Solved string-Extras problems in c#

---
+++ core-csharp-practice/gcr-codebase/c#-strings/string-extras/CheckPalindrome.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace project1.Strings
+//{
+//     class CheckPalindrome
+//    {
+//        //main method
+//                public static void Main(string[] args)
+//        {
+//            Console.WriteLine("Enter a string ");
+
+//            //taking input srting from user
+
+
+//            string s = Console.ReadLine().ToLower();
+
+//            //reversing the string to checkk the string is palindrome
+//            string reverse = "";
+//            for (int i = s.Length - 1; i >= 0; i--)
+//            {
+//                reverse += s[i];
+//            }
+
+//            bool res = s.Equals(reverse);
+
+
+//            Console.WriteLine("the string is palindrom =" + res);
+
+//        }
+
+//    }
+//}
+
+    
---
+++ core-csharp-practice/gcr-codebase/c#-strings/string-extras/MostFrequency.cs
@@ -0,0 +1,35 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace project1.Strings
+//{
+//     class MostFrequency
+//    {
+        
+//            public static void Main(String[] args)
+//            {
+//                Console.Write("Enter a string: ");
+//                string str = Console.ReadLine();
+//                char maximum = str[0];
+//                int max = 0;
+
+//                for (int i = 0; i < str.Length; i++)
+//                {
+//                    int count = 0;
+//                    for (int j = 0; j < str.Length; j++)
+//                    {
+//                        if (str[i] == str[j])
+//                            count++;
+//                    }
+//                    if (count > max)
+//                    {
+//                        max = count;
+//                        maximum = str[i];
+//                    }
+//                }
+//                Console.WriteLine("Most Frequent Character " + maximum);
+//            }
+//        }
+//    }
+
