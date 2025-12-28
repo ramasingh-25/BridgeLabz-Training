ba3813e2d46a81c0568f2d68e43a9186dbcd5527
RamaSingh333 <swatisingh7763@gmail.com>
2025-12-28 22:47:05 +0530

    [Rama] Solved String problems in c#

---
+++ core-csharp-practice/gcr-codebase/c#-strings/level-1/PrintingNullReferenceException.cs
@@ -0,0 +1,46 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace project1.Strings
+//{
+//    class PrintingNullReferenceException
+//    {
+
+//        //main method
+
+
+//    static void Main() {
+//        DemonstrateNullReferenceException();
+//    }
+
+
+//        //method to demonstrate NullReferenceException by accessing a method on a null string.
+
+
+
+//        static void DemonstrateNullReferenceException()
+
+
+
+//    {
+
+
+//        try
+//        {
+
+//            string msg = null;
+            
+//            int length = msg.Length;
+
+//            Console.WriteLine("Length of string: " + length);
+//        }
+
+//        catch (NullReferenceException ex)
+
+//        {
+//            Console.WriteLine("NullReferenceException caught!");
+//            Console.WriteLine("Error Message: " + ex.Message);
+//        }
+//    }    }
+//}
