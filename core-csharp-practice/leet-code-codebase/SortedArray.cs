1c5b7b589d6e25c8f2c058b2744fb4c2ea4be151
RamaSingh333 <swatisingh7763@gmail.com>
2025-12-29 21:47:08 +0530

    [Rama] leetcode Solution

---
+++ core-csharp-practice/leet-code-codebase/SortedArray.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using static System.Runtime.InteropServices.JavaScript.JSType;
+
+//namespace project1.DailyLeetcode
+//{
+//   class SortedArray
+//    {
+       
+//            public boolean check(int[] nums)
+//            {
+//                int count = 0;
+//                boolean check = true;
+//                for (int i = 1; i < nums.Length; i++)
+//                {
+//                    if (nums[i - 1] > nums[i])
+//                    {
+//                        count++;
+//                    }
+
+//                }
+//                if (nums[nums.Length - 1] > nums[0])
+//                {
+//                    count++;
+//                }
+//                return count <= 1;
+
+
+//            }
+//        }
+//    }
