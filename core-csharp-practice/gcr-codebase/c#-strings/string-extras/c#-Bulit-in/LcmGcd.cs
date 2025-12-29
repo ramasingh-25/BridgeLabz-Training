e20ec82644e9e16f5667129dbfb5deb922d4e998
RamaSingh333 <swatisingh7763@gmail.com>
2025-12-29 12:37:36 +0530

    [Rama] Solved string-Extras problems using built-in function in c#

---
+++ core-csharp-practice/gcr-codebase/c#-strings/string-extras/c#-Bulit-in/LcmGcd.cs
@@ -0,0 +1,39 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace project1.Built_in
+//{
+//   class LcmGcd
+//    {
+//        static void Main(String[] args)
+//        {
+//            //taking a input from user
+
+//            Console.Write("Enter first number ");
+//            int a = int.Parse(Console.ReadLine());
+//            Console.Write("Enter second number ");
+//            int b = int.Parse(Console.ReadLine());
+//            Console.WriteLine("GCD: " + GCD(a, b));
+//            Console.WriteLine("LCM: " + LCM(a, b));
+
+//        }
+//        static int GCD(int a, int b)
+
+//        {
+//            while (b != 0)
+//            {
+//                int temp = b;
+//                b = a % b;
+//                a = temp;
+//            }
+//            return a;
+//        }
+//        static int LCM(int a, int b)
+//        {
+//            return (a * b) / GCD(a, b);
+//        }
+
+        
+//    }
+//}
