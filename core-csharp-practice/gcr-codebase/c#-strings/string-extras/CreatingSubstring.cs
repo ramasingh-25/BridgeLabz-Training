c8707ccf19832220fed6a53071027828990d977c
RamaSingh333 <swatisingh7763@gmail.com>
2025-12-29 11:20:09 +0530

    [Rama] Solved string-Extras problems in c#

---
+++ core-csharp-practice/gcr-codebase/c#-strings/string-extras/CreatingSubstring.cs
@@ -0,0 +1,54 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace project1.Strings
+//{
+//     class CreatingSubstring
+    
+//        {
+//            public static string MakingSubstring(string mainString, int firstIndex, int endIndex)
+//            {
+//                string result = "";
+
+//                for (int i = firstIndex; i <= endIndex; i++)
+//                {
+
+
+//                    result += mainString[i];
+
+
+//                }
+//                return result;
+
+//            }
+//            static void Main(string[] args)
+//            {
+//                Console.Write("Enter the string :");
+//                string mainString = Console.ReadLine();
+
+//                Console.WriteLine("Enter the first and last index to create the substring");
+
+//                Console.Write("first index :");
+
+//                int firstIndex = Convert.ToInt32(Console.ReadLine());
+
+//                Console.Write("end index :");
+//                int endIndex = Convert.ToInt32(Console.ReadLine());
+
+//                string custom =  MakingSubstring(mainString, firstIndex, endIndex);
+
+//                string built_in = mainString.Substring(firstIndex, endIndex - firstIndex + 1);
+
+//                Console.WriteLine("Results");
+
+//                Console.WriteLine("Substring created using custom method " + custom);
+//                Console.WriteLine("substring using built in method " + built_in);
+
+
+
+//            }
+//        }
+//    }
+
+
---
+++ core-csharp-practice/gcr-codebase/c#-strings/string-extras/ReplacingWord.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace project1.Strings
+//{
+// class ReplacingWord
+//    {
+//        static void Main()
+//        {
+//            Console.Write("Enter a sentence ");
+//            string str1 = Console.ReadLine();
+//            Console.Write("Enter word to replace: ");
+//            string str2 = Console.ReadLine();
+//            Console.Write("Enter new word: ");
+//            string str3 = Console.ReadLine();
+//            string res = str1.Replace(str2, str3);
+//            Console.WriteLine("Modified Sentence: " + res);
+//        }
+//    }
+//}
