cc84c42502a9c06bc4ff89933b0847b5d9cbd778
Rama Singh <email-used-in-ramasingh-25>
2026-01-05 16:25:04 +0530

    [Rama] Solved scenario based problem in c#

---
+++ oops-csharp-practice/scenario-based/FestiveLottery.cs
@@ -0,0 +1,40 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Oops.Scenario_Based
+//{
+//    internal class FestiveLottery
+//    {
+        
+//            public static void Main(string[] args)
+//            {
+//                string UserChoice = "yes";
+
+//                while (UserChoice == "yes")
+//                {
+//                    Console.Write("Enter your ticket number: ");
+//                    int num = int.Parse(Console.ReadLine());
+
+//                    if (num <= 0)
+//                    {
+//                        Console.WriteLine("Invalid ticket number.");
+//                        continue;
+//                    }
+
+//                    if (num % 7 == 0 && num % 5 == 0)
+//                    {
+//                        Console.WriteLine("Congratulations! You win a gift.");
+//                    }
+//                    else
+//                    {
+//                        Console.WriteLine("Better luck next time.");
+//                    }
+
+//                    Console.Write("Any more visitors? (yes/no): ");
+//                    UserChoice = Console.ReadLine();
+//                }
+//            }
+
+//        }
+//    }
