42f208ad41043dc5e1e217522539cac7dc3fe121
Rama Singh <email-used-in-ramasingh-25>
2026-01-11 23:00:36 +0530

    [Rama] Solved Scenario Based problem in c#

---
+++ oops-csharp-practice/scenario-based/SmartHomeAutomationSystem/Fan.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Xml.Linq;
+
+//namespace Oops.Scenario_Based.SmartHomeAutomationSystem
+//{
+//    internal class Fan : Appliance
+//    {
+//        public Fan(string name) : base(name) { }
+//        public override void TurnOn()
+//        {
+//            Console.WriteLine($"{Name} is turned on in fast speed");
+
+//        }
+//    }
+//}
---
+++ oops-csharp-practice/scenario-based/SmartHomeAutomationSystem/IControllable.cs
@@ -0,0 +1,12 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Oops.Scenario_Based.SmartHomeAutomationSystem
+//{
+//    interface IControllable
+//    {
+//        void TurnOn();
+//        void TurnOff();
+//    }
+//}
