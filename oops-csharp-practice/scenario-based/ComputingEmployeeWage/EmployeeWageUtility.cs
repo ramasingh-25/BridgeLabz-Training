d20e2486d0f7cfd0165d1c678c49b8b48905efcc
Rama Singh <email-used-in-ramasingh-25>
2026-01-12 15:00:39 +0530

    [Rama] Solved Check Employee Attendence [UC1]

---
+++ oops-csharp-practice/scenario-based/ComputingEmployeeWage/EmployeeWageUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oops.Scenario_Based.ComputingEmployeeWage
+{
+    public class EmployeeWageUtility : IEmployeeWages
+    {
+        protected Random random = new Random();
+
+        public void CheckAttendance()
+        {
+            int attendance = random.Next(0, 2);
+
+            if (attendance == 1)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
---
+++ oops-csharp-practice/scenario-based/ComputingEmployeeWage/IEmployeeWages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oops.Scenario_Based.ComputingEmployeeWage
+{
+    public interface IEmployeeWages
+    {
+        void CheckAttendance();
+    }
+}
