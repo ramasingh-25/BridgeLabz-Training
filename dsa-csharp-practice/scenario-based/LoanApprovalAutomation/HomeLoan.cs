8fbed563b7a4c93b18e57c1c21b144916eeb665b
Rama Singh <email-used-in-ramasingh-25>
2026-01-13 10:42:46 +0530

    [Rama] Solved Scenario Based problem in c#

---
+++ dsa-csharp-practice/scenario-based/LoanApprovalAutomation/HomeLoan.cs
@@ -0,0 +1,15 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace BridgeLabzDSA.Scenario_based.LoanApprovalAutomation
+//{
+//    class HomeLoan : LoanApplication
+//    {
+//        public HomeLoan(Applicant applicant, int months) : base(applicant, months, 8.5) { }
+//        public override double CalculateEMI()
+//        {
+//            return base.CalculateEMI() * 0.98;
+//        }
+//    }
+//}
