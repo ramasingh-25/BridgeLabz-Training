075110fb4a82b82da8c43a1d61d8782880cea720
Rama Singh <email-used-in-ramasingh-25>
2026-01-15 12:39:05 +0530

    [Rama] Solved Address Book System [UC1] problem in c#

---
+++ dsa-csharp-practice/scenario-based/AddressBookSystem/AddressBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzDSA.Scenario_based.AddressBookSystem
+{
+    internal class AddressBook
+    {
+        public static void Main (string[] args)
+        {
+
+            Console.WriteLine("Welcome to address book System");
+
+            Contact cont = new Contact ();
+
+            cont.ShowContact();
+
+        }
+       
+
+    }
+}
---
+++ dsa-csharp-practice/scenario-based/AddressBookSystem/Contact.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzDSA.Scenario_based.AddressBookSystem
+{
+
+    internal class Contact
+    {
+        
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Address { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string Zip { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Email { get; set; }
+
+
+            public void ShowContact()
+            {
+                Console.WriteLine("Details");
+                Console.WriteLine("First Name-" + FirstName);
+                Console.WriteLine("Second Name-" + LastName);
+                Console.WriteLine("Address-" + Address);
+                Console.WriteLine("City-" + City);
+                Console.WriteLine("State-" + State);
+                Console.WriteLine("Phone Number-" + PhoneNumber);
+                Console.WriteLine("Email-" + Email);
+
+
+
+            }
+        }
+    }
