4dae90711e1276e6abf84f9d173325ade151a91a
Rama Singh <email-used-in-ramasingh-25>
2026-01-17 12:39:59 +0530

    [Rama] Solved Scenario Based problem in c#

---
+++ dsa-csharp-practice/scenario-based/BookShelf/IBookShelf.cs
@@ -0,0 +1,13 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace BridgeLabzDSA.Scenario_based.BookShelf
+//{
+//    internal interface IBookShelf
+//    {
+//        void AddBook(string genre, string book);
+//        void RemoveBook(string genre, string book);
+//        void Display();
+//    }
+//}
