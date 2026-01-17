3c2692826e9a4a41fa01f69e4d5f09c0e23ef32c
Rama Singh <email-used-in-ramasingh-25>
2026-01-17 12:53:22 +0530

    [Rama] Solved Scenario Based problem in c#

---
+++ dsa-csharp-practice/scenario-based/FlashDealz-QuickSort/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzDSA.Scenario_based.FlashDealz_QuickSort
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int DiscountPercentage { get; set; }
+
+        public Product(string name, int discount)
+        {
+            Name = name;
+            DiscountPercentage = discount;
+        }
+    }
+}
---
+++ dsa-csharp-practice/scenario-based/FlashDealz-QuickSort/RubFlashDealzApp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzDSA.Scenario_based.FlashDealz_QuickSort
+{
+    public class RunFlashDealzApp
+    {
+        public static void Main(string[] args)
+        {
+            IFlashManager manager = new FlashManagerImpl();
+            bool running = true;
+
+            Console.WriteLine("=== FLASH DEALZ MANAGER ===");
+
+            while (running)
+            {
+                FlashUtility.ShowMenu();
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        string name = FlashUtility.GetString("Product Name");
+                        double disc = FlashUtility.GetDouble("Discount %");
+                        manager.AddProduct(name, disc);
+                        break;
+                    case "2":
+                        manager.SortProducts();
+                        break;
+                    case "3":
+                        manager.DisplayDeals();
+                        break;
+                    case "4":
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid");
+                        break;
+                }
+            }
+        }
+    }
+}
