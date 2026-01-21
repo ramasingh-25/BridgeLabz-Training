9907737d78eca6cf7498951144754086092f8269
Rama Singh <email-used-in-ramasingh-25>
2026-01-21 11:10:45 +0530

    [Rama] Solved Scenario Based problem in c#

---
+++ dsa-csharp-practice/scenario-based/PasswordCracker/CrackerMenu.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace BridgelabzCollection.ScenarioBased.PasswordCracker
+//{
+//    public class CrackerMenu
+//    {
+//        public static void Start()
+//        {
+//            Console.Write("Enter password (only z,i,p,7,8): ");
+//            string pass = Console.ReadLine();
+
+//            Cracker crack = new Cracker(pass);
+//            crack.StartCrack();
+
+//            CrackerUtility.ShowComplexity(pass.Length, 5);
+//        }
+//    }
+//}
---
+++ dsa-csharp-practice/scenario-based/SortAadharNumber/AadharRecords.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace BridgelabzCollection.ScenarioBased.SortAadharNumber
+//{
+//    class AadharRecord
+//    {
+//        private long aadhar;
+
+//        public AadharRecord(long a)
+//        {
+//            aadhar = a;
+//        }
+
+//        public long GetAadhar()
+//        {
+//            return aadhar;
+//        }
+//    }
+//}
