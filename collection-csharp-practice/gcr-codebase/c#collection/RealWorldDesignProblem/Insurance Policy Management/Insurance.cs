51009c8b4335028955076714d6157dcbb94f8d57
Rama Singh <email-used-in-ramasingh-25>
2026-01-21 15:41:17 +0530

    [Rama] Solved collection problems in c#

---
+++ collection-csharp-practice/gcr-codebase/c#collection/RealWorldDesignProblem/Insurance Policy Management/Insurance.cs
@@ -0,0 +1,40 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace BridgelabzCollection.RealWorldSystemDesign.Insurance_Policy_Management
+//{
+//    internal class Insurance
+//    {
+//        HashSet<Policy> uniquePolicies = new HashSet<Policy>();
+//        List<Policy> insertionOrder = new List<Policy>();
+//        SortedSet<Policy> sortedByExpiry =
+//            new SortedSet<Policy>(Comparer<Policy>.Create(
+//                (a, b) => a.ExpiryDate.CompareTo(b.ExpiryDate)));
+
+//        public void AddPolicy(Policy policy)
+//        {
+//            if (uniquePolicies.Add(policy))
+//            {
+//                insertionOrder.Add(policy);
+//                sortedByExpiry.Add(policy);
+//            }
+//        }
+
+//        public void PoliciesExpiringSoon()
+//        {
+//            DateTime limit = DateTime.Now.AddDays(30);
+//            foreach (var p in sortedByExpiry)
+//            {
+//                if (p.ExpiryDate <= limit)
+//                    Console.WriteLine(p.PolicyNumber);
+//            }
+//        }
+
+//        public void PoliciesByCoverage(string type)
+//        {
+//            foreach (var p in uniquePolicies.Where(p => p.CoverageType == type))
+//                Console.WriteLine(p.PolicyNumber);
+//        }
+//    }
+//}
---
+++ collection-csharp-practice/gcr-codebase/c#collection/RealWorldDesignProblem/Insurance Policy Management/Policy.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace BridgelabzCollection.RealWorldSystemDesign.Insurance_Policy_Management
+//{
+//    internal class Policy
+//    {
+//        public int PolicyNumber { get; set; }
+//        public string CoverageType { get; set; }
+//        public DateTime ExpiryDate { get; set; }
+
+//        public override bool Equals(object obj)
+//        {
+//            return obj is Policy p && PolicyNumber == p.PolicyNumber;
+//        }
+
+//        public override int GetHashCode()
+//        {
+//            return PolicyNumber.GetHashCode();
+//        }
+//    }
+//}
