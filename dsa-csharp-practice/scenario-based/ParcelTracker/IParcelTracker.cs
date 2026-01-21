9737b0b2a3df37713248d10d51c4cc10b1a9b778
Rama Singh <email-used-in-ramasingh-25>
2026-01-21 10:47:06 +0530

    [Rama] Solved Scenario Based problem in c#

---
+++ dsa-csharp-practice/scenario-based/ParcelTracker/IParcelTracker.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace BridgelabzCollection.ScenarioBased.ParcelTracker
+//{
+//    interface IParcelTracker
+//    {
+//        void AddStage(string stage);
+//        void AddCheckpoint(string afterStage, string newStage);
+//        void TrackForward();
+//        void MarkLost(string stage);
+//    }
+//}
