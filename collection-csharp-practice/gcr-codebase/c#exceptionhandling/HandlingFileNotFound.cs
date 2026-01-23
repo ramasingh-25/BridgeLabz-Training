1e0e02fbfe4119b3cda2c61940aee98d2bf3c362
Rama Singh <email-used-in-ramasingh-25>
2026-01-24 01:14:41 +0530

    [Rama] Solved exception handling problems in c#

---
+++ collection-csharp-practice/gcr-codebase/c#exceptionhandling/HandlingFileNotFound.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace BridgelabzCollection.Exception
+//{
+//    internal class HandlingFileNotFound
+//    {
+//        public static void Main(string[] args)
+//        {
+//            try
+//            {
+
+//                string content = File.ReadAllText("data.txt");
+//                Console.WriteLine("File Contents:");
+//                Console.WriteLine(content);
+//            }
+
+//            catch (IOException)
+//            {
+
+//                Console.WriteLine("File not found");
+//            }
+
+//        }
+//    }
+//}
---
+++ collection-csharp-practice/gcr-codebase/c#exceptionhandling/UsinSystemFileExample.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace BridgelabzCollection.ExceptionHandling
+//{
+//    internal class UsinSystemFileExample
+//    {
+//        public static void Main(string[] args)
+//        {
+//            try
+//            {
+//                using (StreamReader reader = new StreamReader("info.txt"))
+//                {
+//                    string firstLine = reader.ReadLine();
+
+//                    Console.WriteLine(firstLine);
+//                }
+//            }
+//            catch (IOException)
+//            {
+//                Console.WriteLine("Error reading file");
+//            }
+//        }
+//    }
+//}
